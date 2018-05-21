4a62adb2d50b265a04faa1ea03811514222562d5
Nadim Al-Sharif <nadim.al.sharif@gmail.com>
2018-05-21 19:52:01 +0200

    testing

---
+++ src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
+const firebase = require("firebase");
+require("firebase/firestore");
+
+firebase.initializeApp({
+  apiKey: '### FIREBASE API KEY ###',
+  authDomain: '### FIREBASE AUTH DOMAIN ###',
+  projectId: '### CLOUD FIRESTORE PROJECT ID ###'
+});
+const db = firebase.firestore();
+const dbSetting = {timestampsInSnapshots: true};
+db.settings(dbSetting);
+
+
+const citiesRef = db.collection('cities');
+
+citiesRef.doc('SF').set({
+    name: 'San Francisco', state: 'CA', country: 'USA',
+    capital: false, population: 860000 });
+citiesRef.doc('LA').set({
+    name: 'Los Angeles', state: 'CA', country: 'USA',
+    capital: false, population: 3900000 });
+citiesRef.doc('DC').set({
+    name: 'Washington, D.C.', state: null, country: 'USA',
+    capital: true, population: 680000 });
+citiesRef.doc('TOK').set({
+    name: 'Tokyo', state: null, country: 'Japan',
+    capital: true, population: 9000000 });
+citiesRef.doc('BJ').set({
+    name: 'Beijing', state: null, country: 'China',
+    capital: true, population: 21500000 });
+
+    const docRef = db.collection('cities').doc('SF');
+
+    docRef.get().then(function(doc) {
+        if (doc.exists) {
+            console.log('Document data:', doc.data());
+        } else {
+            // doc.data() will be undefined in this case
+            console.log('No such document!');
+        }
+    }).catch(function(error) {
+        console.log('Error getting document:', error);
+    });
